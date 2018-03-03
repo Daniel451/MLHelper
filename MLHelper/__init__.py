ca78717fc6234728ba97de65a6a57e14905d2544
Daniel Speck <daniel@dspeck.de>
2018-03-03 05:10:32 +0100

    new helper functions

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -1,1 +1,3 @@
 from . import noise
+from ._helper_functions import GAI
+from ._helper_functions import PAI
---
+++ MLHelper/_helper_functions.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def PAI(arr : np.ndarray, name : str = "") -> None:
+    """
+    prints array information
+    """
+    print(GAI(arr, name))
+
+
+def GAI(arr : np.ndarray, name : str = "") -> str:
+    """
+    returns formatted array information string
+    """
+    shapestr = str(arr.shape).rjust(20, " ")
+    dtypestr = str(arr.dtype).rjust(10, " ")
+
+    pstr = ""
+    if name != "":
+        pstr = "[{}] ".format(name).ljust(20, " ")
+    pstr += "shape: {} | dtype: {} | min: {:>10.5f} | max: {:>10.5f} | mean: {:>10.5f} | sum: {:>10.2f}" \
+        .format(shapestr, dtypestr, np.min(arr), np.max(arr), np.mean(arr), np.sum(arr))
+
+    return pstr
