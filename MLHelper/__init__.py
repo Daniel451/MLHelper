fc40853e1ac7842fc857319dd8ed5974110bf106
Daniel Speck <daniel451@mailbox.org>
2018-12-26 18:00:54 +0100

    refactoring

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -1,11 +1,16 @@
 from ._helper_functions import GAI
 from ._helper_functions import PAI
+from ._image_label_reader.ImageLabelReader import DataObject as ImgLblReader
+from ._image_label_reader.ImageReader import Reader as ImgReader
 
+# check for OpenCV 3
 try:
-    import cv2
-    s: str = cv2.__version__
-    if not s.startswith("3"):
+    import cv2 as _cv2
+    _s: str = _cv2.__version__
+    if not _s.startswith("3"):
         raise Exception("MLHelper needs OpenCV 3 to work properly")
+    del _cv2
+    del _s
 except Exception as e:
     print(e)
     print("please check your OpenCV installation")
