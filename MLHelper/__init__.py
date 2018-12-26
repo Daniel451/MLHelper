254c62cfffc09b289295b7069c726fde6621a1bf
Daniel Speck <daniel451@mailbox.org>
2018-12-26 17:26:23 +0100

    added reader to MLHelper

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -1,3 +1,11 @@
-from . import noise
 from ._helper_functions import GAI
 from ._helper_functions import PAI
+
+try:
+    import cv2
+    s: str = cv2.__version__
+    if not s.startswith("3"):
+        raise Exception("MLHelper needs OpenCV 3 to work properly")
+except Exception as e:
+    print(e)
+    print("please check your OpenCV installation")
