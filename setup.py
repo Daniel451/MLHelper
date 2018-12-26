d22acd0633e62925a6890a3005dfa554e09377e3
Daniel Speck <daniel451@mailbox.org>
2018-12-26 16:57:07 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(name="MLHelper",
       version="0.11.5",
-      description="Collection of useful helper methods, mostly for manipulating arrays.",
+      description="Collection of image/label readers and useful helper methods, mostly for manipulating arrays.",
       url="https://github.com/Daniel451/MLHelper",
       author="Daniel Speck",
       author_email="daniel451@mailbox.org",
       license="None",
       packages=find_packages(exclude=["contrib", "docs", "build", "dist", "MLHelper.egg-info", "tests*"]),
-      install_requires=["numpy", "scipy"],
+      install_requires=["numpy", "scipy", "tqdm"],
       python_requires=">=3.6",
       zip_safe=False)
