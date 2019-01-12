fa98c6b91213f269966211f0f0fa4a4007e412bd
Daniel Speck <daniel451@mailbox.org>
2019-01-13 00:57:25 +0100

    updated init file

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -3,6 +3,10 @@ from ._helper_functions import PAI
 from ._image_label_reader.ImageLabelReader import DataObject as ImgLblReader
 from ._image_label_reader.ImageReader import Reader as ImgReader
 
+# module structure:
+# * Python 3.3+ treats all folders as packages, thus empty __init__.py files are no longer necessary
+
+
 # check for OpenCV 3
 try:
     import cv2 as _cv2
@@ -14,3 +18,5 @@ try:
 except Exception as e:
     print(e)
     print("please check your OpenCV installation")
+
+
