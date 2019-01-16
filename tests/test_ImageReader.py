1049a29eabab14f59df64e77c5bacda1d04029d4
Daniel Speck <daniel451@mailbox.org>
2019-01-16 18:20:05 +0100

    minor test fix

--- tests/test_ImageReader.py
+++ tests/test_ImageReader.py
@@ -30,7 +30,10 @@ class TestImageReader(unittest.TestCase):
     def test_challenge2018(self):
         dat = H.ImgReader(BallDatasetHandler.TRAIN.CHALLENGE_2018,
                           batch_size=8)
-        self.assertEqual(dat.get_dataset_size(), 35327)
+        # 35327 is the number of ball annotations
+        # self.assertEqual(dat.get_dataset_size(), 35327)
+        # number of images is 35952
+        self.assertEqual(dat.get_dataset_size(), 35952)
 
     def test_batch_size(self):
         batch, paths = TestImageReader.data.get_next_img_batch()
