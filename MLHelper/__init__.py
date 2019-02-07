d78c31c49d272ab670a0ca0b07744766ea2f4c26
Daniel Speck <daniel451@mailbox.org>
2019-02-07 15:06:31 +0100

    added comments to init

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -1,10 +1,14 @@
+# top-level helper
 from ._helper_functions import GAI as GAI
 from ._helper_functions import PAI as PAI
+
+# top-level objects and functions
 from ._image_label_reader.ImageLabelReader import DataObject as ImgLblReader
 from ._image_label_reader.ImageReader import Reader as ImgReader
 from ._image_label_reader.ImageBatch import ImageBatch
 from ._image_label_reader.LabelObjects import Label2D
 
+# submodules
 from . import heatmap
 from . import noise
 
