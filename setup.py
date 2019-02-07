d5eb6c67433090dc56eb14790fa6e7973b974c89
Daniel Speck <daniel451@mailbox.org>
2019-02-07 11:50:41 +0100

    started v0.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="MLHelper",
-      version="0.2.5",
+      version="0.2.6",
       description="Collection of image/label readers and useful helper methods, mostly for manipulating arrays.",
       url="https://github.com/Daniel451/MLHelper",
       author="Daniel Speck",
