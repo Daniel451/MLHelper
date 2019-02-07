0b88577237ff3bc818e4f8f0db6bf6db9ff3ba61
Daniel Speck <daniel451@mailbox.org>
2019-02-07 15:05:06 +0100

    Label2D included in top-level import, OpenCV exit

--- MLHelper/__init__.py
+++ MLHelper/__init__.py
@@ -3,6 +3,7 @@ from ._helper_functions import PAI as PAI
 from ._image_label_reader.ImageLabelReader import DataObject as ImgLblReader
 from ._image_label_reader.ImageReader import Reader as ImgReader
 from ._image_label_reader.ImageBatch import ImageBatch
+from ._image_label_reader.LabelObjects import Label2D
 
 from . import heatmap
 from . import noise
@@ -18,7 +19,7 @@ try:
     del _s
 except Exception as e:
     print(e)
-    print("please check your OpenCV installation")
+    exit("please check your OpenCV installation")
 
 
 
