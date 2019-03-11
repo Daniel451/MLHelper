c8ab070d8688fbfbc9674c32da7d3b3cda4d3e90
Robert <mitachundkrach@gmail.com>
2019-03-11 23:51:14 +0100

    a try to fix it for the moment

--- MLHelper/_image_label_reader/LabelReader.py
+++ MLHelper/_image_label_reader/LabelReader.py
@@ -37,7 +37,6 @@ class Reader:
         for fp in batch_filenames:
             print(self._labels[fp])
             buffer.extend([self._labels[fp]])
-            #counter += 1
         print(buffer)
         return buffer
 
--- MLHelper/heatmap/_labels.py
+++ MLHelper/heatmap/_labels.py
@@ -6,12 +6,10 @@ def labels2D_rectangular(im_batch: np.ndarray, labels):
     labels2D = np.zeros(im_batch.shape[0:3], dtype=np.float32)
     shape_y = im_batch.shape[1]
     shape_x = im_batch.shape[2]
-
+    for k in labels:
+        labels[k] = dict(labels[k])
     # iterate over all batches
     for i, d in enumerate(labels):
-        print(i)
-        print(d)
-        print(labels)
         labels2D[i, d["y1"]: d["y1"] + d["height"], d["x1"]: d["x1"] + d["width"]] = 1.0
 
     return labels2D.reshape(-1, shape_y, shape_x, 1)
