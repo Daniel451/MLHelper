e6e0daa41e35d37f3c8a4b16d3a6f3848417bfa4
Robert <mitachundkrach@gmail.com>
2019-03-12 22:27:40 +0100

    is this real life?

--- MLHelper/heatmap/_labels.py
+++ MLHelper/heatmap/_labels.py
@@ -6,29 +6,14 @@ def labels2D_rectangular(im_batch: np.ndarray, labels):
     labels2D = np.zeros(im_batch.shape[0:3], dtype=np.float32)
     shape_y = im_batch.shape[1]
     shape_x = im_batch.shape[2]
-    # for k in range(0, len(labels)):
-    #     for j in range (0, len(labels[k])):
-    #         labels[k] = dict(labels[k])
-    #iterate over all batches
-    print(labels)
     for i, d in enumerate(labels):
-        print("i:" + str(i) + " d:" + str(d))
+        #print("i:" + str(i) + " d:" + str(d))
         for j in range (0, len(d)):
-            print("j:" + str(j) + "\n d[j]:")
-            print(d[j])
+            #print("j:" + str(j) + "\n d[j]:")
+            #print(d[j])
             b = dict(d[j])
-            print(b)
+            #print(b)
             labels2D[i, b["y1"]: b["y1"] + b["height"], b["x1"]: b["x1"] + b["width"]] = 1.0
-
-    # for i, d in enumerate(labels):
-    #     print("i:" + str(i) + " d:" + str(d))
-    #     for k in range(0, len(labels)):
-    #         print("k:" + str(k))
-    #         for j in range (0, len(labels[k])):
-    #             print("j:" + str(j))
-    #             labels[j] = dict(labels[j])
-    #             print("d:" + str(d) + " i:" + str(i) + " k:" + str(k) + " j:" + str(j))
-    #             labels2D[i, d["y1"]: d["y1"] + d["height"], d["x1"]: d["x1"] + d["width"]] = 1.0
 
     return labels2D.reshape(-1, shape_y, shape_x, 1)
 
@@ -42,10 +27,12 @@ def labels2D_circular(im_batch, labels):
     for i, d in enumerate(labels):
         y = np.arange(0, shape_y)
         x = np.arange(0, shape_x)
+            for j in range (0, len(d)):
+            b = dict(d[j])
 
-        cy = d["center_y"]
-        cx = d["center_x"]
-        r = int(((d["width"] / 2) + (d["height"] / 2)) / 2) + 1
+        cy = b["center_y"]
+        cx = b["center_x"]
+        r = int(((b["width"] / 2) + (b["height"] / 2)) / 2) + 1
 
         mask = (x[np.newaxis, :] - cx) ** 2 + (y[:, np.newaxis] - cy) ** 2 < r ** 2
         labels2D[i][mask] = 1.0
