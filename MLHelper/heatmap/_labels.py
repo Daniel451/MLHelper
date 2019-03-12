df7e1ba3c67a5e880e78d92a22225f1798323c1a
Robert <mitachundkrach@gmail.com>
2019-03-12 21:38:43 +0100

    more output

--- MLHelper/heatmap/_labels.py
+++ MLHelper/heatmap/_labels.py
@@ -6,19 +6,23 @@ def labels2D_rectangular(im_batch: np.ndarray, labels):
     labels2D = np.zeros(im_batch.shape[0:3], dtype=np.float32)
     shape_y = im_batch.shape[1]
     shape_x = im_batch.shape[2]
-    # for k in range(0, len(labels)):
-    #     for j in range (0, len(labels[k])):
-    #         labels[k] = dict(labels[k])
-    # iterate over all batches
+    for k in range(0, len(labels)):
+        for j in range (0, len(labels[k])):
+            labels[k] = dict(labels[k])
+    #iterate over all batches
     for i, d in enumerate(labels):
         print("i:" + str(i) + " d:" + str(d))
-        for k in range(0, len(labels)):
-            print("k:" + str(k))
-            for j in range (0, len(labels[k])):
-                print("j:" + str(j))
-                labels[j] = dict(labels[j])
-                print("d:" + str(d) + " i:" + str(i) + " k:" + str(k) + " j:" + str(j))
-                labels2D[i, d["y1"]: d["y1"] + d["height"], d["x1"]: d["x1"] + d["width"]] = 1.0
+        labels2D[i, d["y1"]: d["y1"] + d["height"], d["x1"]: d["x1"] + d["width"]] = 1.0
+
+    # for i, d in enumerate(labels):
+    #     print("i:" + str(i) + " d:" + str(d))
+    #     for k in range(0, len(labels)):
+    #         print("k:" + str(k))
+    #         for j in range (0, len(labels[k])):
+    #             print("j:" + str(j))
+    #             labels[j] = dict(labels[j])
+    #             print("d:" + str(d) + " i:" + str(i) + " k:" + str(k) + " j:" + str(j))
+    #             labels2D[i, d["y1"]: d["y1"] + d["height"], d["x1"]: d["x1"] + d["width"]] = 1.0
 
     return labels2D.reshape(-1, shape_y, shape_x, 1)
 
