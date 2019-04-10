d474c3eb1d090ae882e23bf803e7ec6bbd8d1f8a
Daniel Speck <daniel451@mailbox.org>
2019-04-10 21:19:36 +0200

    added extra dir

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name="MLHelper",
-      version="0.3.1",
+      version="0.3.2",
       description="Collection of image/label readers and useful helper methods, mostly for manipulating arrays.",
       url="https://github.com/Daniel451/MLHelper",
       author="Daniel Speck",
       author_email="daniel451@mailbox.org",
       license="None",
-      packages=find_packages(exclude=["contrib", "docs", "build", "dist", "MLHelper.egg-info", "tests*"]),
+      packages=find_packages(exclude=["contrib", "docs", "build", "dist", "extra", "MLHelper.egg-info", "tests*"]),
       install_requires=["numpy", "scipy", "tqdm"],
       python_requires=">=3.6",
       zip_safe=False)
