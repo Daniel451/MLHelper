0833f3b46504fd51c8ed2ec91fb9e2a29acc1d13
cpupower <cpupower@users.noreply.github.com>
2019-04-14 19:22:14 +0200

    Refactor tests to use mock datasets and skip tests on real datasets

--- tests/test_ImageReader.py
+++ tests/test_ImageReader.py
@@ -1,27 +1,45 @@
 import unittest
+import os
 import MLHelper as H
 from MLHelper.datasets.bitbots import BallDatasetHandler
+from pathlib import Path
 
 
 class TestImageReader(unittest.TestCase):
     data = None
-
+    test_dir = Path(os.path.realpath(__file__)).parent.absolute()
+    mock_dataset_dir = test_dir.parent / "extra" / "mock-datasets"
+    pathlist = [str(mock_dataset_dir)]
 
     @classmethod
     def setUpClass(cls):
-        TestImageReader.data = H.ImgReader(BallDatasetHandler.TRAIN.LEIPZIG,
-                                           batch_size=8,
+        TestImageReader.data = H.ImgReader(TestImageReader.pathlist,
+                                           batch_size=5,
                                            label_content="ball")
-
 
     def test_instantiation(self):
         self.assertIsNotNone(TestImageReader.data)
 
+    def test_mock_size(self):
+        self.assertEqual(TestImageReader.data.get_dataset_size(), 11)
 
-    def test_leipzig_size(self):
-        self.assertEqual(TestImageReader.data.get_dataset_size(), 14684)
+    def test_mock_ball_label(self):
+        dat = H.ImgReader(TestImageReader.pathlist,
+                          batch_size=5,
+                          label_content="ball",
+                          filter_labels=True)
+        self.assertEqual(dat.get_dataset_size(), 11)
 
+    def test_batch_size(self):
+        batch, paths = TestImageReader.data.get_next_img_batch()
+        self.assertEqual(batch.shape[0], 5)
+        batch, paths = TestImageReader.data.get_next_img_batch()
+        self.assertEqual(batch.shape[0], 5)
+        batch, paths = TestImageReader.data.get_next_img_batch()
+        # FIXME: This fails sometimes. Race condition?
+        self.assertEqual(batch.shape[0], 1)
 
+    @unittest.skip("To be moved to interactive tests")
     def test_leipzig_ball_label(self):
         dat = H.ImgReader(BallDatasetHandler.TRAIN.LEIPZIG,
                           batch_size=8,
@@ -29,19 +47,19 @@ class TestImageReader(unittest.TestCase):
                           filter_labels=True)
         self.assertEqual(dat.get_dataset_size(), 14684)
 
-
+    @unittest.skip("To be moved to interactive tests")
     def test_nagoya_and_size(self):
         dat = H.ImgReader(BallDatasetHandler.TRAIN.NAGOYA,
                           batch_size=8)
         self.assertEqual(dat.get_dataset_size(), 8243)
 
-
+    @unittest.skip("To be moved to interactive tests")
     def test_montreal_and_size(self):
         dat = H.ImgReader(BallDatasetHandler.TRAIN.MONTREAL,
                           batch_size=8)
         self.assertEqual(dat.get_dataset_size(), 16866)
 
-
+    @unittest.skip("To be moved to interactive tests")
     def test_challenge2018(self):
         dat = H.ImgReader(BallDatasetHandler.TRAIN.CHALLENGE_2018,
                           batch_size=8)
@@ -53,8 +71,3 @@ class TestImageReader(unittest.TestCase):
         # 35952 number of images total
         self.assertEqual(dat.get_dataset_size(), 35952)
         self.assertEqual(dat_filtered.get_dataset_size(), 35327)
-
-
-    def test_batch_size(self):
-        batch, paths = TestImageReader.data.get_next_img_batch()
-        self.assertEqual(batch.shape[0], 8)
--- tests/test_LabelReader.py
+++ tests/test_LabelReader.py
@@ -3,28 +3,119 @@ import os
 import time
 import MLHelper as H
 from MLHelper.datasets.bitbots import BallDatasetHandler
+from pathlib import Path
 
 
 class TestLabelReader(unittest.TestCase):
     data = None
-
+    test_dir = Path(os.path.realpath(__file__)).parent.absolute()
+    mock_dataset_dir = test_dir.parent / "extra" / "mock-datasets"
+    pathlist = [str(mock_dataset_dir)]
 
     @classmethod
     def setUpClass(cls):
-        TestLabelReader.data = H.LblReader(BallDatasetHandler.TRAIN.LEIPZIG,
+        TestLabelReader.data = H.LblReader(TestLabelReader.pathlist,
                                            img_dim=(800, 600),
                                            label_content="ball")
-
 
     def test_instantiation(self):
         self.assertIsNotNone(TestLabelReader.data)
 
+    def test_nock_size(self):
+        self.assertEqual(len(TestLabelReader.data.get_set_img()), 11)
 
-    def test_leipzig_size(self):
-        self.assertEqual(len(TestLabelReader.data.get_set_img()), 14684)
+    def test_mock_goalpost(self):
+        dat = H.LblReader(TestLabelReader.pathlist,
+                          img_dim=(800, 600),
+                          label_content="goalpost")
+        imgs = ["img_fr_nov15_000.000.png"]
+        paths = [str(TestLabelReader.mock_dataset_dir / img) for img in imgs]
+        labels = dat.get_labels_for_batch(paths)
+        self.assertEqual(len(labels[0]), 1)
+
+        goalpost = labels[0][0]
+
+        self.assertAlmostEqual(goalpost.x1, 688)
+        self.assertAlmostEqual(goalpost.y1, 0)
+        self.assertAlmostEqual(goalpost.x2, 772)
+        self.assertAlmostEqual(goalpost.y2, 203)
+
+        # calculated in LabelBoundingBox2D - not from label file
+        self.assertAlmostEqual(goalpost.center_x, 730)
+        # calculated in LabelBoundingBox2D - not from label file
+        self.assertAlmostEqual(goalpost.center_y, 101)
+        self.assertAlmostEqual(goalpost.width, 84)
+        self.assertAlmostEqual(goalpost.height, 203)
+
+    def test_mock_multiple_goalpost(self):
+        dat = H.LblReader(TestLabelReader.pathlist,
+                          img_dim=(640, 480),
+                          label_content="goalpost")
+        imgs = ["fake.png"]
+        paths = [str(TestLabelReader.mock_dataset_dir / img) for img in imgs]
+        labels = dat.get_labels_for_batch(paths)
+        goalposts = labels[0]
+        self.assertEqual(len(goalposts), 2, "Should contain two goalpost labels")
+
+        expected_goalposts = [{'x1': 398, 'y1': 0, 'x2': 458, 'y2': 225, 'center_x': 428, 'center_y': 112, 'width': 60, 'height': 225},
+                              {'x1': 197, 'y1': 14, 'x2': 252, 'y2': 236, 'center_x': 224, 'center_y': 125, 'width': 55, 'height': 222}]
+        eps = 1e-2
+        for expected_goalpost in expected_goalposts:
+            matching_goalposts = [gp for gp in goalposts if abs(gp.x1 - expected_goalpost['x1']) < eps]
+            self.assertEqual(len(matching_goalposts), 1)
+            goalposts.remove(matching_goalposts[0])
+            compare_goalpost = {attr: getattr(matching_goalposts[0], attr) for attr in expected_goalpost.keys()}
+            self.assertDictEqual(expected_goalpost, compare_goalpost)
+
+    def test_mock_labels_for_two_images(self):
+        imgs = ["img_fr_nov15_000.003.png", "img_fr_nov15_000.004.png"]
+        paths = [str(TestLabelReader.mock_dataset_dir / img) for img in imgs]
+        labels = TestLabelReader.data.get_labels_for_batch(paths)
+        self.assertEqual(len(labels), 2)
+
+        label_img1 = labels[0][0]
+        label_img2 = labels[1][0]
+
+        # image1
+        # check information
+        self.assertEqual(label_img1.name_set, "bitbots-set00-01")
+        self.assertEqual(label_img1.name_file, imgs[0])
+        # check coordinates
+        self.assertAlmostEqual(label_img1.x1, 353, delta=1)
+        self.assertAlmostEqual(label_img1.y1, 161, delta=1)
+        self.assertAlmostEqual(label_img1.x2, 508, delta=1)
+        self.assertAlmostEqual(label_img1.y2, 324, delta=1)
+        # check image properties
+        self.assertEqual(label_img1.image_width, 800)
+        self.assertEqual(label_img1.image_height, 600)
+        # check bounding box properties
+        self.assertAlmostEqual(label_img1.center_x, 430, delta=1)
+        self.assertAlmostEqual(label_img1.center_y, 242, delta=1)
+        self.assertAlmostEqual(label_img1.width, 155, delta=1)
+        self.assertAlmostEqual(label_img1.height, 163, delta=1)
+
+        # image2
+        # check information
+        self.assertEqual(label_img2.name_set, "bitbots-set00-01")
+        self.assertEqual(label_img2.name_file, imgs[1])
+        # check coordinates
+        self.assertAlmostEqual(label_img2.x1, 292, delta=1)
+        self.assertAlmostEqual(label_img2.y1, 166, delta=1)
+        self.assertAlmostEqual(label_img2.x2, 508, delta=1)
+        self.assertAlmostEqual(label_img2.y2, 380, delta=1)
+        # check image properties
+        self.assertEqual(label_img2.image_width, 800)
+        self.assertEqual(label_img2.image_height, 600)
+        # check bounding box properties
+        self.assertAlmostEqual(label_img2.center_x, 400, delta=1)
+        self.assertAlmostEqual(label_img2.center_y, 273, delta=1)
+        self.assertAlmostEqual(label_img2.width, 216, delta=1)
+        self.assertAlmostEqual(label_img2.height, 214, delta=1)
 
 
-    def test_multiple_goalpost(self):
+
+    @unittest.skip("To be moved to test_interactive")
+    def test_set01_multiple_goalpost(self):
         dat = H.LblReader([os.path.join(os.environ["ROBO_AI_DATA"], "bitbots-set00-01")],
                           img_dim=(800, 600),
                           label_content="goalpost")
@@ -70,7 +161,7 @@ class TestLabelReader(unittest.TestCase):
         self.assertAlmostEqual(goalpost2.width, 68, delta=2)
         self.assertAlmostEqual(goalpost2.height, 123, delta=2)
 
-
+    @unittest.skip("To be moved to test_interactive")
     def test_leipzig_labels_for_two_images(self):
         imgs = ["img_fr_nov15_000.000.png", "img_fr_nov15_000.001.png"]
         paths = [os.path.join(os.environ["ROBO_AI_DATA"], "bitbots-set00-01", i) for i in imgs]
@@ -119,4 +210,3 @@ class TestLabelReader(unittest.TestCase):
         self.assertAlmostEqual(label_img2.center_y, 207, delta=1)
         self.assertAlmostEqual(label_img2.width, 111, delta=1)
         self.assertAlmostEqual(label_img2.height, 110, delta=1)
-
