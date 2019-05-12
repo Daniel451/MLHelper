5fe6b7881cd2494342c768883ffa047e27c5da44
Daniel Speck <daniel451@mailbox.org>
2019-05-12 15:11:49 +0200

    added bs4 & requests to dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name="MLHelper",
       author_email="daniel451@mailbox.org",
       license="None",
       packages=find_packages(exclude=["contrib", "docs", "build", "dist", "extra", "MLHelper.egg-info", "tests*"]),
-      install_requires=["numpy", "scipy", "tqdm"],
+      install_requires=["numpy", "scipy", "tqdm", "bs4", "requests"],
       python_requires=">=3.6",
       zip_safe=False)
